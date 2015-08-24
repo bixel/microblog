bc6f7e9922046caf4b148580ab9048b088a58442
Kevin Heinicke <kevin@kehei.de>
2015-08-24 16:06:14 +0200

    export port to config

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,9 @@ import config
 UPLOAD_PATH = 'app/static/img/upload/'
 
 app = Flask(__name__)
-app.config['COUCHDB_SERVER'] = 'http://%s%s@localhost:21902/' % (config.username, ':' + config.password)
+app.config['COUCHDB_SERVER'] = ('http://{}:{}@localhost:{}/'
+                                .format(config.username,
+                                        config.password,
+                                        config.port))
 app.config['COUCHDB_DATABASE'] = 'soak'
 app.config['UPLOAD_PATH'] = UPLOAD_PATH
-from app import views
--- run.py
+++ run.py
@@ -1,3 +1,3 @@
 #!bin/python
-from app import app
+from app.views import app
 app.run(debug=True)
